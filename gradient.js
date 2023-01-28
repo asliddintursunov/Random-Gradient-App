829eefea7d36654ce93c08992df620983fa19983
asliddintursunov <asliddintursunovfarkhodovich@gmail.com>
2023-01-29 02:06:23 +0500

    first version

---
+++ gradient.js
@@ -0,0 +1,44 @@
+let body = document.querySelector('body');
+let container = document.querySelector('.container');
+let colorText = document.querySelector('.color-text');
+
+let values = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f'
+];
+
+const gradientColor = () => {
+    let randomColor1 = '#';
+    let randomColor2 = '#';
+    for(let i = 0; i < 6; i++){
+        let ran1 = Math.floor(Math.random() * 16);
+        let ran2 = Math.floor(Math.random() * 16);
+
+        randomColor1 += values[ran1];
+        randomColor2 += values[ran2];
+    }
+    // console.log(randomColor1);
+    // console.log(randomColor2);
+
+    let randomDegree = Math.floor(Math.random() * 360) + 'deg';
+    // console.log(randomDegree);
+
+    body.style = `background-image: linear-gradient(${randomDegree}, ${randomColor1}, ${randomColor2})`;
+
+    colorText.textContent = `linear-gradient (${randomDegree}, ${randomColor1}, ${randomColor2})`;
+}
+container.onclick = () => gradientColor()
